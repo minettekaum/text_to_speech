c2a36c9fe4b84d4e608d1be724a06227be4f74b9
Minette <minette@Eriks-MacBook-Air.local>
2025-06-10 15:16:04 +0200

    fixing port

--- backend/main.py
+++ backend/main.py
@@ -104,7 +104,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://*.koyeb.app", "http://localhost:5173"],
+    allow_origins=["https://soft-lexine-challenge-d3e578f4.koyeb.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
