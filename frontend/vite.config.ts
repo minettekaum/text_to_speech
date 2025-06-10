312ea94c2a6aeff3de0643aac58324a484785b25
minettebrink <mimmi.brink@gmail.com>
2025-06-10 09:14:12 +0200

    fixing ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		host: true,
+		port: 4173,
+		strictPort: true,
+		allowedHosts: ['soft-lexine-challenge-d3e578f4.koyeb.app']
+	}
 });
